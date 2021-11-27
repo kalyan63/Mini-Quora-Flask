c2da78d5a4711552733be902d46b13c1c33aa2f7
kalyan63 <skalyanreddy123@gmail.com>
2021-11-27 17:13:26 +0530

    Mini Quora Final

---
+++ Sql_Querries.py
@@ -0,0 +1,241 @@
+import sqlite3
+import os
+import hashlib
+from datetime import datetime
+
+def CheckUser(Uname):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Count(*) from User where User_name = ?",(Uname,))
+    ct=cur.fetchone()
+    conn.close()
+    return(ct[0])
+
+def CheckMail(Mail):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Count(*) from User where Email = ?",(Mail,))
+    ct=cur.fetchone()
+    conn.close()
+    return(ct[0])
+
+def CheckVotes(A_id,user_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Count(*) from Votes where A_id = ? and user_id= ?",(A_id,user_id))
+    ct=cur.fetchone()
+    conn.close()
+    return(ct[0])
+
+def Check_Follow(U_id,F_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Count(*) from Follow where user_id = ? and follow_id = ?",(U_id,F_id))
+    ct=cur.fetchone()
+    conn.close()
+    return(ct[0])
+
+def FollowUser(U_id,F_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Insert into Follow values(?,?,?)",(U_id,F_id,str(datetime.now())))
+    conn.commit()
+    conn.close()
+
+def UnfollowUser(U_id,F_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Delete from Follow where user_id = ? and follow_id = ?",(U_id,F_id))
+    conn.commit()
+    conn.close()
+    
+def InsertUser(Uname,Mail,Password):
+    salt=os.urandom(8)
+    hash=hashlib.pbkdf2_hmac('sha256', Password.encode('utf-8'), salt, 100000)
+    Cur_date=str(datetime.now())
+    arg=(Uname,hash,salt,Mail,Cur_date)
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Insert into User(User_name,Password,Salt,Email,LastLogin) values(?,?,?,?,?)",arg)
+    conn.commit()
+    conn.close()
+
+def CheckLogin(Uname,Password):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Password,Salt from User where User_name=?",(Uname,))
+    res=cur.fetchone()
+    conn.close()
+    if(not res):
+        return 0
+    DbPassword,salt=res[0],res[1]
+    if(hashlib.pbkdf2_hmac('sha256', Password.encode('utf-8'), salt, 100000)==DbPassword):
+        return 1
+    else:
+        return 0
+
+def GetUserId(Uname):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select user_id from User where User_name=?",(Uname,))
+    res=cur.fetchone()
+    conn.close()
+    return res[0]
+
+def GetUserName(U_Id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select User_name from User where user_id=?",(U_Id,))
+    res=cur.fetchone()
+    conn.close()
+    return res[0]
+
+def GetUserMail(U_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Email from User where user_id=?",(U_id,))
+    res=cur.fetchone()
+    conn.close()
+    return res[0]
+
+def GetUserDetails(U_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("select user_id,user_name,email from User where user_id = ?", (U_id,))
+    res=cur.fetchall()
+    conn.close()
+    return res[0]
+
+def GetUserQues(U_id):
+    conn = sqlite3.connect('quora.db') 
+    cur = conn.cursor() 
+    cur.execute("select Q_id,Q_text,Date from Questions where user_id=? order by Date Desc",(U_id,)) 
+    data = cur.fetchall()  
+    conn.close()
+    return(data)
+
+def GetUserAns(U_id):
+    conn = sqlite3.connect('quora.db') 
+    cur = conn.cursor() 
+    cur.execute("select A_id,Q_id,A_text,Date from Answer where user_id=? order by Date Desc",(U_id,)) 
+    data = cur.fetchall()  
+    conn.close()
+    return(data)
+def GetFollowId(U_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select follow_id from Follow where user_id = ?",(U_id,))
+    data = cur.fetchall()  
+    conn.close()
+    return(data)
+
+def DisplayQues(): 
+    conn = sqlite3.connect('quora.db') 
+    cur = conn.cursor() 
+    cur.execute("select * from Questions") 
+    data = cur.fetchall()  
+    conn.close()
+    return(data)
+
+def InsertQues(user_id,Q_Text,An):
+    Cur_date=str(datetime.now())
+    arg=(user_id,Q_Text,An,Cur_date)
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Insert into Questions(user_id,Q_Text,Anonymous,Date) values(?,?,?,?)",arg)
+    conn.commit()
+    conn.close()
+
+def GetQuestion(Q_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select * from Questions where Q_id=?",(Q_id,))
+    Ques=cur.fetchall()
+    cur.execute("Select * from Answer where Q_id=?",(Q_id,))
+    Ans=cur.fetchall()
+    conn.close()
+    return {"Ques":Ques[0],"Ans":Ans}
+
+def GetAnswer(A_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select * from Answer where A_id=?",(A_id,))
+    Ans=cur.fetchall()
+    cur.execute("Select * from Comments where A_id=?",(A_id,))
+    Com=cur.fetchall()
+    conn.close()
+    return {"Ans":Ans[0],"Com":Com}
+
+def GetMailQuestion(Q_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select user_id from Questions where Q_id=?",(Q_id,))
+    U_id=cur.fetchone()
+    conn.close()
+    return GetUserMail(U_id[0])
+
+def InsertAns(Q_id,user_id,A_text,An):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Insert into Answer(Q_id,user_id,A_Text,Date,Anonymous) values(?,?,?,?,?)",(Q_id,user_id,A_text,str(datetime.now()),An))
+    conn.commit()
+    cur.execute("Select A_id from Answer where Q_id=? and user_id=? and A_text=?",(Q_id,user_id,A_text))
+    res=cur.fetchone()
+    conn.close()
+    return res[0]
+
+def InsertCom(A_id,user_id,C_Text,An):
+    arg=(A_id,user_id,C_Text,An)
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Insert into Comments(A_id,user_id,Text,Anonymous) values(?,?,?,?)",arg)
+    conn.commit()
+    conn.close()
+
+def VoteCount(A_id,V_type):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select Count(*) from Votes where A_id = ? and vote_type = ?",(A_id,V_type))
+    ct=cur.fetchone()
+    conn.close()
+    return(ct[0])
+
+def UpdateVotes(A_id):
+    U_c=VoteCount(A_id,1)
+    D_c=VoteCount(A_id,0)
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Update Answer set Upvote_count=?, Downvote_count=? where A_id=?",(U_c,D_c,A_id)) 
+    conn.commit()
+    conn.close()
+
+def UserVoteAns(user_id,A_id,V_type):
+    if(CheckVotes(A_id,user_id)):
+        conn=sqlite3.connect('quora.db')
+        cur=conn.cursor()
+        cur.execute("Update Votes set vote_type=? where A_id=? and user_id=?",(V_type,A_id,user_id))
+    else:
+        conn=sqlite3.connect('quora.db')
+        cur=conn.cursor()
+        cur.execute("Insert into Votes(A_id,user_id,vote_type) values(?,?,?)",(A_id,user_id,V_type)) 
+    conn.commit()
+    conn.close()   
+    UpdateVotes(A_id)
+
+def InsertFiles(A_id,loc):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Insert into Media(A_id,File_loc) values(?,?)",(A_id,loc))
+    conn.commit()
+    conn.close()
+
+def GetFileLoc(A_id):
+    conn=sqlite3.connect('quora.db')
+    cur=conn.cursor()
+    cur.execute("Select File_loc from Media where A_id=?",(A_id,))
+    res=cur.fetchone()
+    conn.close()
+    if(res):
+        return res[0]
+    else:
+        return None    
---
+++ app.py
@@ -0,0 +1,156 @@
+from flask import Flask,render_template,redirect,request,session
+from datetime import datetime
+from flask_mail import Mail, Message
+from Sql_Querries import Check_Follow, CheckUser,CheckMail,CheckLogin,\
+FollowUser, GetFileLoc, InsertFiles,InsertUser,DisplayQues,GetUserId,\
+GetUserDetails,GetMailQuestion,GetUserQues,GetUserAns,GetFollowId,InsertQues,GetQuestion,GetAnswer,InsertAns,\
+GetUserName, UnfollowUser,UserVoteAns,InsertCom,InsertFiles,GetFileLoc
+import os
+app=Flask(__name__)
+app.secret_key = "cn assignment safty key **&**"
+app.config['UPLOAD_FOLDER']='ImgFolder/'
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'chiluverupreeti@gmail.com' # _________________ADD YOUR EMAIL ID AND PASSWORD _____________
+app.config['MAIL_PASSWORD'] = 'pReeticH23' # _______________ADD YOUR EMAIL ID AND PASSWORD __________________
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEBUG'] = True
+app.config['MAIL_SUPPRESS_SEND'] = False
+app.config['TESTING'] = False
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('Login.html')
+
+@app.route('/Login',methods=['POST','GET'])
+def Login():
+    if request.method=="POST":
+        Uname=request.form['Uname']
+        Password=request.form['Password']
+        if(CheckLogin(Uname,Password)):
+            session['UserId']=GetUserId(Uname)
+        else:
+            print("Wrong Username and password")
+            return redirect('/')
+    return render_template('Home.html',Questions=DisplayQues(),get_Name=GetUserName)
+
+@app.route('/Signup',methods=['POST','GET'])
+def Signup():
+    if(request.method=="POST"):
+        Uname=request.form['Uname']
+        Password=request.form['Password']
+        Email=request.form['Email']
+        if(CheckUser(Uname)):
+            print("User already present")
+        elif(CheckMail(Email)):
+            print("Mail already taken")
+        else:
+            InsertUser(Uname,Email,Password)        
+    return render_template('Login.html')    
+
+@app.route('/Home',methods=["POST","GET"])
+def Home():
+    if(request.method=="POST"):
+        Q_Text=request.form['q_text']
+        An=int(request.form.get('anonymous',0))   
+        user_id=session['UserId']
+        if(Q_Text):
+            InsertQues(user_id,Q_Text,An)
+        else:
+            print("Please enter a question") 
+        return render_template('Home.html',Questions=DisplayQues(),get_Name=GetUserName)    
+    if(not session.get('UserId',None)):
+        return render_template('Login.html')
+    else:
+        return render_template('Home.html',Questions=DisplayQues(),get_Name=GetUserName)
+
+@app.route('/Logout')
+def Logout():
+    session.pop('UserId',None)
+    return render_template('Login.html')
+
+@app.route('/profile/<int:user_Id>')
+def Profile(user_Id):
+    if(user_Id!=session['UserId']):
+        return render_template('Profile.html',user=GetUserDetails(user_Id),foll=Check_Follow(session['UserId'],user_Id))
+    else:    
+        return render_template('Profile.html',user=GetUserDetails(user_Id),get_Name=GetUserName,F_ID=GetFollowId(user_Id))
+
+@app.route('/profile/follow/<int:user_Id>')
+def Follow(user_Id):
+    FollowUser(session['UserId'],user_Id)
+    return render_template('Profile.html',user=GetUserDetails(user_Id),foll=Check_Follow(session['UserId'],user_Id))
+
+@app.route('/profile/unfollow/<int:user_Id>')
+def UnFollow(user_Id):
+    UnfollowUser(session['UserId'],user_Id)
+    return render_template('Profile.html',user=GetUserDetails(user_Id),foll=Check_Follow(session['UserId'],user_Id))
+
+@app.route('/profile/Question/<int:user_id>')
+def UserQuestionList(user_id):
+    Aut=[]
+    Aut.append(session['UserId']==user_id)
+    Aut.append(GetUserName(user_id))
+    return render_template('UserQuestionList.html',Questions=GetUserQues(user_id),Aut=Aut)
+
+@app.route('/profile/Answer/<int:user_id>')
+def UserAnswerList(user_id):
+    Aut=[]
+    Aut.append(session['UserId']==user_id)
+    Aut.append(GetUserName(user_id))
+    return render_template('UserAnswerList.html',Answers=GetUserAns(user_id),Aut=Aut)
+
+@app.route('/Answer/<int:A_id>',methods=["POST","GET"])
+def Answer(A_id):
+    if(request.method=="POST"):
+        C_Text=request.form['C_text']
+        An=int(request.form.get('anonymous',0))   
+        user_id=session['UserId']
+        if(C_Text):
+            InsertCom(A_id,user_id,C_Text,An)
+        else:
+            print("Please enter a question") 
+        return render_template('Answer.html',Ans_Com=GetAnswer(A_id),Get_File=GetFileLoc,get_Name=GetUserName)     
+    else:
+        return render_template('Answer.html',Ans_Com=GetAnswer(A_id),Get_File=GetFileLoc,get_Name=GetUserName)          
+
+@app.route('/Question/<int:Q_id>',methods=["POST","GET"])
+def Question(Q_id):
+    if(request.method=="POST"):
+        A_txt=request.form['A_text']
+        An=int(request.form.get('anonymous',0))
+        file=request.files['file']
+        if(A_txt):
+            A_id=InsertAns(Q_id,session['UserId'],A_txt,An)
+            if(file):
+                f_path=os.path.join(app.config['UPLOAD_FOLDER'],file.filename)
+                file.save("static/"+f_path)
+                InsertFiles(A_id,f_path)
+            Own_mail=GetMailQuestion(Q_id)   
+            try:
+                msg = Message( 'Hello from mini-quora!', sender ='chiluverupreeti@gmail.com', recipients = [str(Own_mail)])
+                msg.body = 'Hello!! You got a message from Mini-quora! Your Posted question has been answered'
+                mail.send(msg) 
+            except:
+                print("Invalid user mail")    
+        else:
+            print("Answer is blank")
+        return render_template('Question.html',Question=GetQuestion(Q_id),Get_File=GetFileLoc,get_Name=GetUserName)
+    else:    
+        return render_template('Question.html',Question=GetQuestion(Q_id),Get_File=GetFileLoc,get_Name=GetUserName)
+
+@app.route('/Question/UpVotes/<int:A_id>',methods=["POST","GET"])
+def UpVote(A_id):
+    UserVoteAns(session['UserId'],A_id,1)
+    return render_template('Answer.html',Ans_Com=GetAnswer(A_id),Get_File=GetFileLoc,get_Name=GetUserName)
+
+@app.route('/Question/DownVotes/<int:A_id>',methods=["POST","GET"])
+def DownVote(A_id):
+    UserVoteAns(session['UserId'],A_id,0)
+    return render_template('Answer.html',Ans_Com=GetAnswer(A_id),Get_File=GetFileLoc,get_Name=GetUserName)
+
+if __name__=="__main__":
+    app.run(debug=True)
